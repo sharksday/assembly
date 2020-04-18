d72d89f477c9dfe60116b0213118337b4e659347
sharksday <kassmon@kassmon.com>
2020-04-18 14:09:37 -0700

    finished subroutine command

--- asm/com/kassmon/assembly/program/commands/Jsr.java
+++ asm/com/kassmon/assembly/program/commands/Jsr.java
@@ -4,11 +4,12 @@ import static com.kassmon.library.log.Log.newLogEntry;
 
 import com.kassmon.assembly.logic.RunTime;
 import com.kassmon.assembly.program.Argument;
+import com.kassmon.assembly.program.Program;
 import com.kassmon.assembly.tokenizer.Tokenizer;
 import com.kassmon.library.log.EntryType;
 
 public class Jsr extends Command {
-	private String path = "com.kassmon.assembly.program.commands.Rsr";
+	private String path = "com.kassmon.assembly.program.commands.Jsr";
 	
 	@Override
 	public Command parse(Tokenizer t) {
@@ -36,7 +37,15 @@ public class Jsr extends Command {
 	
 	@Override
 	public void run(RunTime runtime) {
-		
+		runtime.pushToStack(runtime.getPc());
+		Program program = runtime.getProgram();
+		for (int i = 0; i < program.getProgramLength(); i++) {
+			if (!program.getProgramLine(i).isCommand()) {
+				if (program.getProgramLine(i).getLabel().equals(a1.getValue())) {
+					runtime.setPc(i);
+				}
+			}
+		}
 	}
 	
 }
--- asm/com/kassmon/assembly/program/commands/Rsr.java
+++ asm/com/kassmon/assembly/program/commands/Rsr.java
@@ -1,32 +1,18 @@
 package com.kassmon.assembly.program.commands;
 
 import com.kassmon.assembly.logic.RunTime;
-import com.kassmon.assembly.program.Argument;
 import com.kassmon.assembly.tokenizer.Tokenizer;
-import com.kassmon.library.log.EntryType;
-import static com.kassmon.library.log.Log.newLogEntry;
 
 public class Rsr extends Command {
-	private String path = "com.kassmon.assembly.program.commands.Rsr";
 	
 	@Override
 	public Command parse(Tokenizer t) {
-		Argument a1 = super.getArg(t);
-		if (!a1.isLabel())
-			return new Rsr(a1);
-		newLogEntry(EntryType.ERROR, path, "not a valid argument");
-		return null;
+		return new Rsr();
 	}
 	
 	@Override
 	public String getPattern() {
 		return "rsr";
-	}
-	
-	private Argument a1;
-	
-	public Rsr(Argument a1) {
-		this.a1 = a1;
 	}
 	
 	public Rsr() {
@@ -35,7 +21,7 @@ public class Rsr extends Command {
 	
 	@Override
 	public void run(RunTime runtime) {
-		
+		runtime.setPc(runtime.popFromStack());
 	}
 	
 }
